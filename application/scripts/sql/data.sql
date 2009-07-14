be19ed1614dc40c647e79bda18a89a920805f6d9
Lee Boynton <notlee@gmail.com>
2009-07-14 20:53:46 +0100

    Change data SQL script to use delete from instead of truncate so that it works with SQLite

--- application/scripts/sql/data.sql
+++ application/scripts/sql/data.sql
@@ -1,6 +1,6 @@
 -- empty tables first
-truncate users;
-truncate user_details;
+delete from users;
+delete from user_details;
 
 -- insert test data
 insert into users (username, password, openid, created, user_type) VALUES ('testuser', 'a45adc8ea23392523a6431f16fbdea9d', 'http://localhost/?user=testuser', '2009-01-01 00:00:00+00', 'member');
