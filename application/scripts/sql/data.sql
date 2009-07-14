fe7ff8438a2225602a22c952de73d37c7ec16cf0
Lee Boynton <notlee@gmail.com>
2009-07-14 14:55:27 +0100

    Add truncate commands to the data SQL script so that it doesn't try to enter duplicate data.

--- application/scripts/sql/data.sql
+++ application/scripts/sql/data.sql
@@ -1,2 +1,7 @@
+-- empty tables first
+truncate users;
+truncate user_details;
+
+-- insert test data
 insert into users (username, password, openid, created, user_type) VALUES ('testuser', 'a45adc8ea23392523a6431f16fbdea9d', 'http://localhost/?user=testuser', '2009-01-01 00:00:00+00', 'member');
-insert into user_details (user_id, key, value) VALUES (1, 'email', 'test@test.com');
+insert into user_details (user_id, key, value) VALUES ((select id from users where username = 'testuser'), 'email', 'test@test.com');
