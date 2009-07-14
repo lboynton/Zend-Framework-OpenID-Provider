f1facf6157778495a9ee1f5bae76a24fcf51bfb8
Lee Boynton <notlee@gmail.com>
2009-07-14 18:24:40 +0100

    Allowed trusted sites to be deleted

---
+++ application/models/Site.php
@@ -0,0 +1,128 @@
+<?php
+/* 
+ * 
+ * $Id$
+ * 
+ * Software License Agreement (BSD License)
+ * 
+ * Copyright (c) 2009, University of Portsmouth
+ * All rights reserved.
+ * 
+ * Redistribution and use of this software in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *   Redistributions of source code must retain the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer.
+ * 
+ *   Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer in the documentation and/or other
+ *   materials provided with the distribution.
+ * 
+ *   Neither the name of University of Portsmouth nor the names of its
+ *   contributors may be used to endorse or promote products
+ *   derived from this software without specific prior
+ *   written permission of University of Portsmouth
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Description of Site
+ *
+ * @author Lee Boynton
+ */
+class Default_Model_Site
+{
+    protected $_id;
+    protected $_site;
+    protected $_time;
+    protected $_trusted;
+    protected $_openid;
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    public function setId($id)
+    {
+        $this->_id = $id;
+    }
+
+    public function getSite()
+    {
+        return $this->_site;
+    }
+
+    public function setSite($site)
+    {
+        $this->_site = $site;
+    }
+
+    public function getTime()
+    {
+        return $this->_time;
+    }
+
+    public function setTime($time)
+    {
+        $this->_time = $time;
+    }
+
+    public function getTrusted()
+    {
+        return $this->_trusted;
+    }
+
+    public function setTrusted($trusted)
+    {
+        $this->_trusted = $trusted;
+    }
+
+    public function getOpenid()
+    {
+        return $this->_openid;
+    }
+
+    public function setOpenid($openid)
+    {
+        $this->_openid = $openid;
+    }
+    
+    public function find($id)
+    {
+        $this->getMapper()->find($id, $this);
+        return $this;
+    }
+
+    public function delete($id, $openid = null)
+    {
+        if(is_null($openid)) $openid = Zend_Auth::getInstance()->getIdentity()->openid;
+
+        $this->getMapper()->delete($id, $openid);
+    }
+
+    public function getMapper()
+    {
+        if (null === $this->_mapper)
+        {
+            $this->setMapper(new Default_Model_SiteMapper());
+        }
+        return $this->_mapper;
+    }
+
+    public function setMapper($mapper)
+    {
+        $this->_mapper = $mapper;
+        return $this;
+    }
+}
---
+++ application/models/SiteMapper.php
@@ -0,0 +1,78 @@
+<?php
+/* 
+ * 
+ * $Id$
+ * 
+ * Software License Agreement (BSD License)
+ * 
+ * Copyright (c) 2009, University of Portsmouth
+ * All rights reserved.
+ * 
+ * Redistribution and use of this software in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *   Redistributions of source code must retain the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer.
+ * 
+ *   Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer in the documentation and/or other
+ *   materials provided with the distribution.
+ * 
+ *   Neither the name of University of Portsmouth nor the names of its
+ *   contributors may be used to endorse or promote products
+ *   derived from this software without specific prior
+ *   written permission of University of Portsmouth
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Description of SiteMapper
+ *
+ * @author Lee Boynton
+ */
+class Default_Model_SiteMapper extends Default_Model_DataMapper
+{
+    public function find($id, Default_Model_Site $site)
+    {
+        $result = $this->getDbTable()->find($id);
+
+        if (0 == count($result))
+        {
+            return;
+        }
+
+        $row = $result->current();
+
+        $site->setId($row->id);
+        $site->setSite($row->site);
+        $site->setTrusted($row->trusted);
+        $site->setDate($row->date);
+        $site->setOpenid($row->openid);
+    }
+
+    public function delete($id, $openid)
+    {
+        $where[] = $this->getDbTable()->getAdapter()->quoteInto('id = ?', $id);
+        $where[] = $this->getDbTable()->getAdapter()->quoteInto('openid = ?', $openid);
+        $this->getDbTable()->delete($where);
+    }
+
+    public function getDbTable()
+    {
+        if (null === $this->_dbTable)
+        {
+            $this->setDbTable('Default_Model_DbTable_Site');
+        }
+        return $this->_dbTable;
+    }
+}
