fabd81884978bedf38570f2f1c1e93c83531fe25
Lee Boynton <notlee@gmail.com>
2009-07-15 15:13:03 +0100

    Update load schema script so that default application environment is production and ensure that library directory is on include path.

--- application/scripts/loadSchema.php
+++ application/scripts/loadSchema.php
@@ -43,7 +43,13 @@
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__)) . '/../');
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', 'testing');
+    || define('APPLICATION_ENV', 'production');
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
 
 require_once 'Zend/Application.php';
 $application = new Zend_Application(
